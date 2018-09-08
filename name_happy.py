c7b5f722eb092a87efa54e8738ceb59600231974
SamMillerlic <aimiller@outlook.com>
2018-09-08 22:04:01 +0800

    the first python file to be uploaded

---
+++ name_happy.py
@@ -0,0 +1,6 @@
+name = ' albert einstein '
+sentence = '"A person who never made a mistake never tried anything new."'
+comment = "It is his quate."
+#print(name.title().rstrip()+' had said \n\t'+sentence+" \n\t"+comment)
+print(name.strip())
+print(name)
